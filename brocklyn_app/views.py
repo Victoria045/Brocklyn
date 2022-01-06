db76feaf132888cc86dc7eab20792c20a62eef67
Victoria045 <victoriaberyl45@gmail.com>
2022-01-06 20:04:45 +0300

    create the register view

--- brocklyn_app/views.py
+++ brocklyn_app/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render,redirect, get_object_or_404
 from django.http import HttpResponse, Http404,HttpResponseRedirect, JsonResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
+from django.contrib.auth.models import User
 
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'registration/register.html', {'form': form})
 
 def index(request):
 
