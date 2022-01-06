b03ffd84672353ea167d445210d6265111bb1942
Victoria045 <victoriaberyl45@gmail.com>
2022-01-06 20:04:00 +0300

    create the register url routes

--- brocklyn_app/urls.py
+++ brocklyn_app/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path,include
 from . import views
-# from django.contrib.auth import views as auth_views
+from django.contrib.auth import views as auth_views
 # from rest_framework import routers
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.index, name='index'),
+    path('index/',views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('',auth_views.LoginView.as_view(), name='login'),
+    path('account/', include('django.contrib.auth.urls')),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
